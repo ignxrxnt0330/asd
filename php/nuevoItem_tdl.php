eb1cf9de439f1eb50ca532bdfc0699ba9f8ef2ac
Edmilson Da Silva <dsm.edmilson@gmail.com>
2023-12-08 18:06:16 +0100

    Now you are unable to submit an empty item to the to-do list, either the title or the text has to be set

--- php/nuevoItem_tdl.php
+++ php/nuevoItem_tdl.php
@@ -3,8 +3,9 @@ require("C:/xampp/htdocs/asd/php/Todolist.php");
 $titulo = $_POST['titulo'];
 $descripcion = $_POST['descripcion'];
 $fecha = $_POST['fecha'];
-
-Todolist::insertar($titulo,$descripcion,$fecha);
+if(!($titulo==""&&$descripcion=="")){
+    Todolist::insertar($titulo,$descripcion,$fecha);
+}
 
 header("Location: http://localhost/pags/todolist.php");
 exit();
