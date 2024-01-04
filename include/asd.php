f68939f99e2f0109df2fd866886605fb533e323a
Edmilson Da Silva <dsm.edmilson@gmail.com>
2024-01-04 12:25:30 +0100

    Fixed randomSplash() not working in home

--- include/asd.php
+++ include/asd.php
@@ -1,23 +1,18 @@
-<?php
-
-
-?>
-
-
 <link rel="stylesheet" type="text/css" href="../css/a1.css">
-<link rel="favicon" type="image/png" href="../imgs/favicon.png">
+<link rel="icon" type="image/png" href="../imgs/favicon.png">
 <meta charset="utf-8">
 </head>
+
 <body>
-<header>
-    <span id="egg">
-        <!---marquee con asdasdasd que vaya con el hover del span-->
-        <!--- asd randoms que aparecen con dif posiciones, tamaños y º de rotacion al hacer click en el span--->
+    <header>
+        <span id="egg">
+            <!---marquee con asdasdasd que vaya con el hover del span-->
+            <!--- asd randoms que aparecen con dif posiciones, tamaños y º de rotacion al hacer click en el span--->
         </span>
         <span>
             <?php
             $pageName = pathinfo(basename($_SERVER['PHP_SELF']), PATHINFO_FILENAME);
-            if($pageName!="index")
+            if ($pageName != "index")
                 echo $pageName;
             else
                 echo "<a href='https://github.com/ignxrxnt0330/asd/' target='_blank'> asd </a>";
@@ -26,54 +21,51 @@
     </header>
     <div id="todo">
         <div id="barra_lateral">
-        <div id="item">
-            <a href="/pags/index.php">Home
-				<ul id="subitems">
-				</ul>
-            </a>
-	</div>
-    
-    <div id="item">
-            <a href="/pags/urbanDic.php">urbanDic
-				<ul id="subitems">
-				</ul>
-            </a>
-	</div>
-  
-    <div id="item">
-            <a href="/pags/todolist.php">
-                To-do list
-            </a>
-	</div>
-
-    
-    <div id="item">
-            <a href="../typing_game/typing_game.php">
-                Typing game
-				<ul id="subitems">
-				</ul>
-            </a>
-	</div>
-
-     
-    <div id="item">
-            <a href="">
-                Code
-				<ul id="subitems">
-				</ul>
-            </li> </a>
-	</div>
-
-    <div id="item">
-            <a href="">
-                mrds
-				<ul id="subitems">
-					<li><a href="">mrds</a></li>
-					<li><a href="">asd</a></li>
-					<li><a href="">xdf</a></li>
-				</ul>
+            <div id="item">
+                <a href="/pags/index.php">Home
+                    <ul id="subitems">
+                    </ul>
+                </a>
             </div>
-        </a>
-    </div>
 
-</html>
+            <div id="item">
+                <a href="/pags/urbanDic.php">urbanDic
+                    <ul id="subitems">
+                    </ul>
+                </a>
+            </div>
+
+            <div id="item">
+                <a href="/pags/todolist.php">
+                    To-do list
+                </a>
+            </div>
+
+
+            <div id="item">
+                <a href="../typing_game/typing_game.php">
+                    Typing game
+                    <ul id="subitems">
+                    </ul>
+                </a>
+            </div>
+
+            <div id="item">
+                <a href="">
+                    Code
+                    <ul id="subitems">
+                    </ul>
+                </a>
+            </div>
+
+            <div id="item">
+                <a href="">
+                    mrds
+                    <ul id="subitems">
+                        <li><a href="">mrds</a></li>
+                        <li><a href="">asd</a></li>
+                        <li><a href="">xdf</a></li>
+                    </ul>
+                </a>
+            </div>
+        </div>
--- pags/index.php
+++ pags/index.php
@@ -1,36 +1,7 @@
-<script defer>
-    function randomSplash() {
-        fetch('http://localhost/php/splashes.php')
-            //browsers restrict fetching from local files for security reasons, so it has to be accessed this way
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('response is not ok');
-                }
-                return response.json(); // parse the response as JSON to a JS object, as this is what the ajax request returns
-            })
-            .then(data => {
-                const splash = data.splash;
-                if (splash.trim() !== "") {// not empty
-                    document.querySelector('.splash').textContent = splash;
-                    document.title = splash;
-                    console.log(splash);
-                } else {                    // empty
-                    console.error("empty splash");
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
-
-    randomSplash();
-</script>
-
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/index.css">
@@ -46,40 +17,44 @@
 
         <div class="new_splash">
             <form>
-                <input type="text" class="new_splash_input" name="splash" placeholder="new_splash"
-                    autocomplete="off"></input>
+                <input type="text" class="new_splash_input" placeholder="new_splash" autocomplete="off" />
             </form>
         </div>
 
-        <script defer>
-            let input = document.querySelector(".new_splash_input");
-            input.addEventListener("keydown", function (event) {
-                if (event.key == "Enter") {
-                    event.preventDefault();
-                    $.ajax({
-                        type: 'POST',
-                        url: 'http://localhost/php/newSplash.php',
-                        data: { splash: input.value },
-                        success: function (response) {
-                            console.log(response);
-                            console.log("Splash added correctly");
-                        },
-                        error: function (error) {
-                            console.error("Error adding splash: " + error);
-                        }
-                    });
-                    input.value = "";
-                }
-            });
-        </script>
+        <div class="lineCount"></div>
 
-        <div class=lineCount></div>
         <script defer>
+            function randomSplash() {
+                fetch('http://localhost/php/splashes.php')
+                    //browsers restrict fetching from local files for security reasons, so it has to be accessed this way
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('response is not ok');
+                        }
+                        return response.json(); // parses the response as JSON to a JS object, as this is what the ajax request returns
+                    })
+                    .then(data => {
+                        const splash = data.splash;
+                        if (splash.trim() !== "") {// not empty
+                            document.querySelector('.splash').textContent = splash;
+                            document.title = splash;
+                            console.log(splash);
+                        } else {                    // empty
+                            console.error("empty splash");
+                        }
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            }
+
+            randomSplash();
+
             document.querySelector('.splash').addEventListener("click", function () {
-                console.log("asd");
                 randomSplash();
             });
         </script>
+
         <script defer>
             fetch('http://localhost/php/lineCount.php')
                 .then(response => {
@@ -101,7 +76,29 @@
                     console.error(error);
                 });
         </script>
-        
+
+        <script defer>
+            let input = document.querySelector('.new_splash_input');
+            input.addEventListener("keydown", function (event) {
+                if (event.key == "Enter") {
+                    event.preventDefault();
+                    $.ajax({
+                        type: 'POST',
+                        url: 'http://localhost/php/newSplash.php',
+                        data: { splash: input.value },
+                        success: function (response) {
+                            console.log(response);
+                            console.log("Splash added correctly");
+                        },
+                        error: function (error) {
+                            console.error("Error adding splash: " + error);
+                        }
+                    });
+                    input.value = "";
+                }
+            });
+        </script>
+
     </div>
 
     </body>
